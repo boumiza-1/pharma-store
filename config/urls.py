37db4adb75d6ed3e552daccaf2197821db2aa3d6
Amine <amineboumiza2011@gmail.com>
2026-08-16 04:28:59 +0300

    Fix Render media files

--- config/urls.py
+++ config/urls.py
@@ -3,13 +3,21 @@ from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
 
+
 urlpatterns = [
+    # Django Admin
     path("admin/", admin.site.urls),
+
+    # Store application
     path("", include("store.urls")),
 ]
 
-if settings.DEBUG:
-    urlpatterns += static(
-        settings.MEDIA_URL,
-        document_root=settings.MEDIA_ROOT
-    )
+
+# ============================================================
+# MEDIA FILES
+# ============================================================
+
+urlpatterns += static(
+    settings.MEDIA_URL,
+    document_root=settings.MEDIA_ROOT,
+)
